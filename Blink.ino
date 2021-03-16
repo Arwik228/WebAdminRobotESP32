374dab28beaa7faf22f24b79e4a5fb2fe35c97ef
Arwik228 <den_lebedev_2018@list.ru>
2021-03-16 22:58:22 +0500

    0.0.1

---
+++ Blink.ino
@@ -0,0 +1,347 @@
+#include "string.h"
+
+#include "WiFi.h"
+
+#include "SPIFFS.h"
+
+#include "AsyncTCP.h"
+
+#include "esp32-hal-ledc.h"
+
+#include "ESPAsyncWebServer.h"
+
+/*
+   Set a username and password for your home network.
+   If everything is correct, the site will be available at 192.168.1.199
+*/
+const char* ssid = "ASUS";
+const char* password = "123456789";
+/*
+    Set logical pins that will be responsible for one direction or another for
+   each of the wheelsю
+*/
+#define leftWheelForwardPIN 27
+#define rightWheelForwardPIN 12
+#define leftWheelBackwardPIN 14
+#define rightWheelBackwardPIN 26
+/*
+    If the body of the machine was assembled incorrectly and it drifts to one
+   side.
+    It is necessary to accelerate the wheel of the side to which it is moving.
+    The motor speed scale is 0/255, the base machine speed is 100 for 5 volts,
+   so the speed of the skid side will be higher.
+    Note: Only 1 value out of 2 is allowed. Acceleration is matched to your
+   voltage and base speed
+*/
+#define randomTurnLeft 20 //%
+#define randomTurnRight 0 //%
+/*
+   Install the pins of the collision sensors, the code is for 3 sensors.
+   left, center, right sensors
+*/
+#define laserSensorLeftPIN 16
+#define laserSensorCenterPIN 17
+#define laserSensorRightPIN 5
+/*
+   Default speed 0-255, 150/180
+   Const 8 holes whell sensor
+   Wheel Circumference
+   Pin counter rotate L, R
+*/
+#define defaultSpeed 100
+#define whellSensor 8
+#define wheelCircumference 18.85 // cm
+#define countRotationLeftPIN 18
+#define countRotationRightPIN 19
+/*
+    If you have a buzzer with an installed generator, then use this pin.
+    Else set this variable to false
+*/
+#define speakerPIN 22
+/*
+   Some esp32 boards have touch button outputs.
+   You can wake up from sleep mode by clicking on it
+   Else set this variable to false
+   D4  = T0 | D2  = T2 | D15 = T3
+   D13 = T4 | D12 = T5 | D14 = T6
+   D27 = T7 | D33 = T8 | D32 = T9
+*/
+#define touchPIN T0
+
+//*********************************************\\
+
+File file;
+int counterLeft = 0, counterRight = 0, speedLeft = 0, speedRight = 0, touchValue = 100,
+    threshold = 50,  speed = 0 ;
+int drive = 0;
+bool driveState = false, oldStateCounterLeft = false, oldStateCounterRight = false,
+     oldValueTouch = false, speakerState = false;
+
+IPAddress local_IP(192, 168, 1, 199);
+IPAddress gateway(192, 168, 1, 1);
+IPAddress subnet(255, 255, 255, 0);
+IPAddress primaryDNS(8, 8, 8, 8);
+IPAddress secondaryDNS(8, 8, 4, 4);
+
+AsyncWebServer server(80);
+AsyncWebSocket ws("/websocket");
+
+void speaker()
+{
+  for (int i = 0; i < 10; i++)
+  {
+    digitalWrite(speakerPIN, HIGH);
+    delay(100);
+    digitalWrite(speakerPIN, LOW);
+    delay(100);
+  }
+  digitalWrite(speakerPIN, LOW);
+  speakerState = false;
+}
+
+void onWsEvent(AsyncWebSocket* server, AsyncWebSocketClient* client, AwsEventType type, void* arg,
+               uint8_t* data, size_t len)
+{
+  if (type == WS_EVT_CONNECT)
+  {
+    client->text("Websocket client connection received");
+  }
+  else if (type == WS_EVT_DISCONNECT)
+  {
+    Serial.println("Client disconnected");
+  }
+  else if (type == WS_EVT_DATA)
+  {
+    if (!strncmp((const char*)data, "find", 4))
+    {
+      if (speakerPIN)
+      {
+        speakerState = true;
+      }
+    }
+    else if (!strncmp((const char*)data, "goAuto", 6))
+    {
+      driveState = true;
+    }
+    else if (!strncmp((const char*)data, "stopAuto", 8))
+    {
+      driveState = false;
+    }
+    else if (!strncmp((const char*)data, "goHome", 6))
+    {
+      drive = 4;
+    }
+    else if (!strncmp((const char*)data, "clearStory", 10))
+    {
+      if (SPIFFS.remove("/config.ini"))
+      {
+        client->text("Clear file story");
+      }
+      else
+      {
+        Serial.print("Deleting file failed!");
+      }
+    }
+    else if (!strncmp((const char*)data, "alg-random", 10))
+    {
+      drive = 1;
+    }
+    else if (!strncmp((const char*)data, "alg-contour", 11))
+    {
+      drive = 2;
+    }
+    else if (!strncmp((const char*)data, "alg-snake", 9))
+    {
+      drive = 3;
+    }
+    else if (!strncmp((const char*)data, "u", 1))
+    {
+      speed = data[1] - '0';
+      speed = speed > 5 ? 5 : (speed < 1 ? 1 : speed);
+      ledcWrite(1, (255 / 100 * (100 - randomTurnLeft) / 5 * speed));
+      ledcWrite(2, 0);
+      ledcWrite(3, 0);
+      ledcWrite(0, (255 / 100 * (100 - randomTurnRight) / 5 * speed));
+    }
+    else if (!strncmp((const char*)data, "l", 1))
+    {
+      speed = data[1] - '0';
+      speed = speed > 5 ? 5 : (speed < 1 ? 1 : speed);
+      ledcWrite(1, (255 / 100 * (100 - randomTurnLeft) / 5 * speed));
+      ledcWrite(2, 0);
+      ledcWrite(3, (255 / 100 * (100 - randomTurnRight) / 5 * speed));
+      ledcWrite(0, 0);
+    }
+    else if (!strncmp((const char*)data, "r", 1))
+    {
+      speed = data[1] - '0';
+      speed = speed > 5 ? 5 : (speed < 1 ? 1 : speed);
+      ledcWrite(1, 0);
+      ledcWrite(2, (255 / 100 * (100 - randomTurnLeft) / 5 * speed));
+      ledcWrite(3, 0);
+      ledcWrite(0, (255 / 100 * (100 - randomTurnRight) / 5 * speed));
+    }
+    else if (!strncmp((const char*)data, "d", 1))
+    {
+      speed = data[1] - '0';
+      speed = speed > 5 ? 5 : (speed < 1 ? 1 : speed);
+      ledcWrite(1, 0);
+      ledcWrite(2, (255 / 100 * (100 - randomTurnLeft) / 5 * speed));
+      ledcWrite(3, (255 / 100 * (100 - randomTurnRight) / 5 * speed));
+      ledcWrite(0, 0);
+    }
+    else if (!strncmp((const char*)data, "stopJoy", 7))
+    {
+      ledcWrite(1, 0);
+      ledcWrite(2, 0);
+      ledcWrite(3, 0);
+      ledcWrite(0, 0);
+    }
+  }
+}
+
+void IRAM_ATTR laserLeftFunction()
+{
+  if (driveState)
+  {
+    ledcWrite(1, 0);
+    ledcWrite(2, 0);
+    ledcWrite(3, 0);
+    ledcWrite(0, 255);
+  }
+}
+
+void IRAM_ATTR laserCenterFunction()
+{
+  if (driveState)
+  {
+    ledcWrite(1, 0);
+    ledcWrite(3, 0);
+    ledcWrite(0, 255);
+    ledcWrite(2, 255);
+  }
+}
+
+void IRAM_ATTR laserRightFunction()
+{
+  if (driveState)
+  {
+    ledcWrite(0, 0);
+    ledcWrite(2, 0);
+    ledcWrite(3, 0);
+    ledcWrite(1, 255);
+  }
+}
+
+void IRAM_ATTR countLeftFunction()
+{
+  counterLeft++;
+}
+
+void IRAM_ATTR countRightFunction()
+{
+  counterRight++;
+}
+
+void initPIN()
+{
+  ledcSetup(0, 50, 8);
+  ledcAttachPin(leftWheelForwardPIN, 0);
+  ledcAttachPin(rightWheelForwardPIN, 1);
+  ledcAttachPin(leftWheelBackwardPIN, 2);
+  ledcAttachPin(rightWheelBackwardPIN, 3);
+  pinMode(leftWheelForwardPIN, OUTPUT);
+  pinMode(rightWheelForwardPIN, OUTPUT);
+  pinMode(leftWheelBackwardPIN, OUTPUT);
+  pinMode(rightWheelBackwardPIN, OUTPUT);
+  pinMode(laserSensorLeftPIN, INPUT_PULLUP);
+  pinMode(laserSensorCenterPIN, INPUT_PULLUP);
+  pinMode(laserSensorRightPIN, INPUT_PULLUP);
+  pinMode(countRotationLeftPIN, INPUT_PULLUP);
+  pinMode(countRotationRightPIN, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(laserSensorLeftPIN), laserLeftFunction, FALLING);
+  attachInterrupt(digitalPinToInterrupt(laserSensorCenterPIN), laserCenterFunction, FALLING);
+  attachInterrupt(digitalPinToInterrupt(laserSensorRightPIN), laserRightFunction, FALLING);
+  attachInterrupt(digitalPinToInterrupt(countRotationLeftPIN), countLeftFunction, FALLING);
+  attachInterrupt(digitalPinToInterrupt(countRotationRightPIN), countRightFunction, FALLING);
+  if (speakerPIN)
+  {
+    pinMode(speakerPIN, OUTPUT);
+  }
+  speedLeft = 80 + (80 / 100 * randomTurnLeft);
+  speedRight = 80 + (80 / 100 * randomTurnRight);
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  initPIN();
+  if (randomTurnLeft ^ randomTurnRight)
+  {
+    if (randomTurnLeft)
+    {
+      speedLeft = defaultSpeed;
+      speedRight = defaultSpeed - (defaultSpeed / 100 * randomTurnLeft);
+    }
+    else
+    {
+      speedRight = defaultSpeed;
+      speedLeft = defaultSpeed - (defaultSpeed / 100 * randomTurnRight);
+    }
+  }
+  else
+  {
+    Serial.println("You can specify only one speed correction out of 2");
+  }
+  if (!SPIFFS.begin(true))
+  {
+    Serial.println("An Error has occurred while mounting SPIFFS");
+    return;
+  }
+  file = SPIFFS.open("/config.ini", "w+");
+  if (!file)
+  {
+    Serial.println("There was an error opening the file for writing");
+    return;
+  }
+  WiFi.begin(ssid, password);
+  if (!WiFi.config(local_IP, gateway, subnet, primaryDNS))
+  {
+    Serial.println("STA Failed to configure");
+  }
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+  Serial.println(WiFi.localIP());
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest * request)
+  {
+    request->send(SPIFFS, "/index.html", String(), false);
+  });
+  server.on("/style.css", HTTP_GET, [](AsyncWebServerRequest * request)
+  {
+    request->send(SPIFFS, "/style.css", "text/css");
+  });
+  server.on("/main.js", HTTP_GET, [](AsyncWebServerRequest * request)
+  {
+    request->send(SPIFFS, "/main.js", "text/javascript");
+  });
+  server.on("/favicon.png", HTTP_GET, [](AsyncWebServerRequest * request)
+  {
+    request->send(SPIFFS, "/favicon.png", "image/png");
+  });
+  ws.onEvent(onWsEvent);
+  server.addHandler(&ws);
+  server.begin();
+}
+
+void loop()
+{
+  if(speakerState){
+    speaker();
+  }
+
+
+  delay(10);
+}
---
+++ data/main.js
@@ -0,0 +1,218 @@
+var textTemplate = {
+    start: "Подключаюсь...",
+    open: "Соединение установлено",
+    stop: "Соединение закрыто",
+    error: "Ошибка соединения",
+    reload: "Соединение перезагружается",
+    find: "Подаю звуковой сигнал...",
+    ready: "Ожидаю команду",
+    noselect: "Режим не выбран"
+}
+var ws, mapArray = [],
+    text = textTemplate.start,
+    joy;
+
+function socket() {
+    ws = new WebSocket(`ws://${window.location.host || "192.168.1.199"}/websocket`);
+    ws.onopen = (e) => {
+        document.getElementById("status").innerText = "Online";
+        document.getElementById("status").style.color = "lime";
+        text = textTemplate.open;
+    }
+    ws.onclose = (e) => {
+        document.getElementById("status").innerText = "Offline";
+        document.getElementById("status").style.color = "red";
+        text = textTemplate.stop;
+    }
+    ws.onmessage = (e) => {
+        try {
+            let message = eval(`(${e.data})`);
+            let io1 = document.getElementById("io1");
+            let io2 = document.getElementById("io2");
+            let io3 = document.getElementById("io3");
+            if (message.center !== undefined) {
+                message.left ? (
+                    io1.innerText = "false",
+                    io1.style.color = "lime"
+                ) : (
+                    io1.innerText = "true",
+                    io1.style.color = "red"
+                );
+                message.center ? (
+                    io2.innerText = "false",
+                    io2.style.color = "lime"
+                ) : (
+                    io2.innerText = "true",
+                    io2.style.color = "red"
+                );
+                message.right ? (
+                    io3.innerText = "false",
+                    io3.style.color = "lime"
+                ) : (
+                    io3.innerText = "true",
+                    io3.style.color = "red"
+                );
+            }
+        } catch (j) {
+            console.log(e.data)
+        }
+        text = textTemplate.ready;
+    }
+    ws.onerror = (e) => {
+        document.getElementById("status").innerText = "Offline";
+        document.getElementById("status").style.color = "red";
+        text = textTemplate.error;
+    }
+}
+
+function reconect() {
+    if (ws.close) {
+        ws.close(), ws = false, setTimeout(socket, 2500);
+    }
+}
+
+function find() {
+    text = textTemplate.find, ws.send("find");
+}
+
+function go() {
+    let input = document.querySelector('input[name="contact"]:checked');
+    if (input) {
+        text = textTemplate.ready
+        if (ws.readyState === 1) {
+            ws.send("goAuto");
+        } else {
+            text = textTemplate.error;
+        }
+    } else {
+        text = textTemplate.noselect;
+    }
+}
+
+function stop() {
+    if (ws.readyState === 1) {
+        ws.send("stopAuto");
+    } else {
+        text = textTemplate.error;
+    }
+}
+
+function goHome() {
+    if (ws.readyState === 1) {
+        ws.send("goHome");
+    } else {
+        text = textTemplate.error;
+    }
+}
+
+function alg(e) {
+    let input = document.querySelector('input[name="contact"]:checked');
+    if (input) {
+        text = textTemplate.ready
+        if (ws.readyState === 1) {
+            ws.send([`alg-${input.value}`]);
+        } else {
+            text = textTemplate.error;
+        }
+    } else {
+        text = textTemplate.noselect;
+    }
+}
+
+function clearStory() {
+    if (ws.readyState === 1) {
+        ws.send("clearStory");
+    } else {
+        text = textTemplate.error;
+    }
+}
+
+function joystickRender() {
+    let send = oldValue = oldSpeed = false;
+    joy = new JoyStick('joyDiv', {
+        title: 'joystick',
+        width: 150,
+        height: 150,
+        internalFillColor: 'orange',
+        internalLineWidth: 2,
+        internalStrokeColor: 'orange',
+        externalLineWidth: 2,
+        externalStrokeColor: '#e91e63',
+        autoReturnToCenter: true
+    });
+    setInterval(() => {
+        if (ws.readyState === 1) {
+            let localJoyX = joy.GetPosX();
+            let localJoyY = joy.GetPosY()
+            if (75 != localJoyX || 75 != localJoyY) {
+                let absX = Math.abs(localJoyX - 75);
+                let absY = Math.abs(localJoyY - 75);
+                if (absX > absY) {
+                    let localSpeed = Math.round(absX / 10);
+                    if (localJoyX > 90 && (oldValue != "r" || localSpeed != oldSpeed)) {
+                        ws.send(`r${localSpeed}`);
+                        oldValue = "r";
+                        oldSpeed = localSpeed;
+                    }
+                    if (localJoyX < 60 && (oldValue != "l" || localSpeed != oldSpeed)) {
+                        ws.send(`l${localSpeed}`);
+                        oldValue = "l";
+                        oldSpeed = localSpeed;
+                    }
+                } else {
+                    let localSpeed = Math.round(absY / 10);
+                    if (localJoyY > 90 && (oldValue != "d" || localSpeed != oldSpeed)) {
+                        ws.send(`d${localSpeed}`);
+                        oldValue = "d";
+                        oldSpeed = localSpeed;
+                    }
+                    if (localJoyY < 60 && (oldValue != "u" || localSpeed != oldSpeed)) {
+                        ws.send(`u${localSpeed}`);
+                        oldValue = "u";
+                        oldSpeed = localSpeed;
+                    }
+                }
+                send = true;
+            } else {
+                if (send) {
+                    oldValue = "stopJoy";
+                    ws.send("stopJoy");
+                    send = false;
+                }
+            }
+        } else {
+            text = textTemplate.error;
+        }
+    }, 150);
+}
+
+window.onload = () => {
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
+    render();
+
+    function render() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        for (let i = 0; i < 64; i++) {
+            for (let j = 0; j < 100; j++) {
+                if (mapArray[i] && mapArray[i][j]) {
+                    let w = i * 8;
+                    let h = j * 8
+                    ctx.fillStyle = '#7188d9'
+                    ctx.fillRect(h, w, h + 8, w + 8);
+                } else {
+                    let w = i * 8;
+                    let h = j * 8
+                    ctx.fillStyle = '#1d1d1d'
+                    ctx.fillRect(h, w, h + 8, w + 8);
+                }
+            }
+        }
+        ctx.fillStyle = "orange";
+        ctx.font = "18px cursive";
+        ctx.fillText(`Информация: ${text}`, 10, 25)
+    }
+    setInterval(render, 500);
+    socket();
+    joystickRender();
+}
